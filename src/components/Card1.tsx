defd9f0a0df49918a0b855d1fc6e93c025f43916
Arinze Owoh <arinzeowoh@gmail.com>
2024-09-25 04:08:24 +0100

    initial commit: import some icons, import and configure fonts, create card components

---
+++ src/components/Card1.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+interface Card1Props {
+  label: string;
+  value: string;
+  iconSrc: string;
+  url: string;
+  percentage: number;
+}
+
+const Card1 = ({ label, value, iconSrc, url, percentage }: Card1Props) => {
+  return (
+    <div className="h-[150px] w-[268px] bg-[white] rounded-[8px] p-[24px] font-montserrat flex flex-col justify-between">
+      <div className="flex items-start gap-[14px]">
+        <Image src={iconSrc} alt="Card Icon" width={44} height={44} />
+        <div className=" font-[500]">
+          <p className="text-[16px] leading-[1.37] mb-[5px]">{label}</p>
+          <p className=" text-[24px] leading-[1.37]">{value}</p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center font-[400] text-[14px] leading-[1.43]">
+        <Link href={url}>
+          <span className="text-[#0F7863] ">View All</span>
+          <Image
+            src={"/images/icon-view-all.png"}
+            alt="View all"
+            width={16}
+            height={16}
+            className="inline-block ml-[5px]"
+          />
+        </Link>
+        <div className="bg-[rgba(11,138,0,0.15)] rounded-[50px] py-[3px] px-[5px]">
+          <Image
+            src={"/images/icon-trending-up.png"}
+            alt="Trending up"
+            width={14}
+            height={14}
+            className="inline-block mr-[4px]"
+          />
+          <span className="text-[#0B8A00]">{`${percentage.toFixed(1)}%`}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card1;
---
+++ src/components/Card2.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+interface Card2Props {
+  label: string;
+  value: string;
+  iconSrc: string;
+  url: string;
+}
+
+const Card2 = ({ label, value, iconSrc, url }: Card2Props) => {
+  return (
+    <div className="h-[150px] w-[268px] bg-[white] rounded-[8px] p-[24px] font-montserrat flex flex-col justify-between">
+      <div className="flex items-start gap-[14px]">
+        <Image src={iconSrc} alt="Card Icon" width={44} height={44} />
+        <div className=" font-[500]">
+          <p className="text-[16px] leading-[1.37] mb-[5px]">{label}</p>
+          <p className=" text-[24px] leading-[1.37]">{value}</p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center font-[400] ">
+        <Link href={url}>
+          <span className="text-[#0F7863] text-[14px] leading-[1.43]">
+            View All
+          </span>
+          <Image
+            src={"/images/icon-view-all.png"}
+            alt="View all"
+            width={16}
+            height={16}
+            className="inline-block ml-[5px]"
+          />
+        </Link>
+        <button className="bg-[#F1F3F4] rounded-[4px] px-[3px]">
+          <span className="text-[#2B2E48] text-[12px] leading[1.37] ml-[8px]">
+            Month
+          </span>
+          <Image
+            src={"/images/icon-month-dropdown.png"}
+            alt="Month dropdown"
+            width={16}
+            height={16}
+            className="inline-block ml-[2px]"
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card2;
