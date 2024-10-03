e7ccc87a1f8a916c5c12b226beaf0b24d2ec1b26
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-03 07:33:44 +0100

    implement cutsom layout DashboardLayout.tsx for all dashboard inner pages

---
+++ src/app/dashboard/DashboardLayout.tsx
@@ -0,0 +1,34 @@
+import "@/app/globals.css";
+import RevenueStreamIcon from "@/components/icons/RevenueStreamIcon";
+import Image from "next/image";
+import Link from "next/link";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="bg-[#354B45] flex items-center justify-between mb-[14px]">
+        <span className="font-roboto font-[500] text-[20px] text-[#FFFFFF] leading-[1.37] flex gap-[10px] items-center ml-[25px]">
+          <RevenueStreamIcon />
+          Revenue Stream
+        </span>
+        <Link
+          href={"/dashboard"}
+          className="w-[50px] h-[50px] flex items-center justify-center bg-[#21332E]"
+        >
+          <Image
+            src={"/images/icon-close.png"}
+            width={21}
+            height={21}
+            alt="Close"
+          />
+        </Link>
+      </div>
+
+      <div className="w-[97.4%] mx-[auto] overflow-y-auto pt-[10px] pb-[69px]  no-scrollbar">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
