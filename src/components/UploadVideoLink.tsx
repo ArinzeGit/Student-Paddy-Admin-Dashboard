22f37448758d45dca7eb02cb2b4bee0ed5576ebb
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-05 07:29:40 +0100

    minor edit

--- src/components/UploadVideoLink.tsx
+++ src/components/UploadVideoLink.tsx
@@ -7,12 +7,7 @@ const UploadVideoLink = () => {
       href={""}
       className="text-[#FFFFFF] text-[14px] font-roboto font-[900] leading-[1.37] bg-[#5D4C80] rounded-[30px] px-[19px] py-[12px] flex gap-[11px] items-center"
     >
-      <Image
-        src={"/images/icon-addition.png"}
-        alt="Add"
-        height={9}
-        width={9}
-      ></Image>
+      <Image src={"/images/icon-addition.png"} alt="Add" height={9} width={9} />
       Upload video
     </Link>
   );
