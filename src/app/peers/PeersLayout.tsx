37948001e664581e2ebd97adb8e364721cfaf289
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-06 05:11:39 +0100

    reimplement PeersLayout to grey out and disable main nav tabs while in inner peer pages

--- src/app/peers/PeersLayout.tsx
+++ src/app/peers/PeersLayout.tsx
@@ -6,37 +6,42 @@ import Link from "next/link";
 
 const PeersLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex flex-col">
-      <div className="bg-[#354B45] flex items-center justify-between">
-        <span className="font-roboto font-[500] text-[20px] text-[#FFFFFF] leading-[1.37] flex gap-[10px] items-center ml-[25px]">
-          <PeersIcon />
-          Peers
-        </span>
-        <Link
-          href={"/peers"}
-          className="w-[50px] h-[50px] flex items-center justify-center bg-[#21332E]"
-        >
-          <Image
-            src={"/images/icon-close.png"}
-            width={21}
-            height={21}
-            alt="Close"
+    <>
+      <div className="fixed inset-0 bg-[#000000] opacity-40" />
+      {/*relative to overlap fixed div and bg since <body> background is covered by fixed div*/}
+      <div className="h-screen flex flex-col bg-[#F1F3F4] relative">
+        <div className="bg-[#354B45] flex items-center justify-between">
+          <span className="font-roboto font-[500] text-[20px] text-[#FFFFFF] leading-[1.37] flex gap-[10px] items-center ml-[25px]">
+            <PeersIcon />
+            Peers
+          </span>
+          <Link
+            href={"/peers"}
+            className="w-[50px] h-[50px] flex items-center justify-center bg-[#21332E]"
+          >
+            <Image
+              src={"/images/icon-close.png"}
+              width={21}
+              height={21}
+              alt="Close"
+            />
+          </Link>
+        </div>
+        <div className="w-[97.4%] mx-[auto] mb-[14px] mt-[24px] flex gap-[14px]">
+          <CustomLink href="/peers/total-peers" label="Total Peers" />
+          <CustomLink href="/peers/top-peers" label="Top Peers" />
+          <CustomLink href="/peers/inactive-peers" label="Inactive Peers" />
+          <CustomLink href="/peers/active-peers" label="Active Peers" />
+          <CustomLink
+            href="/peers/recommended-peers"
+            label="Recommended Peers"
           />
-        </Link>
+        </div>
+        <div className="w-[97.4%] mx-[auto] overflow-y-auto pt-[10px] pb-[69px]  no-scrollbar">
+          {children}
+        </div>
       </div>
-
-      <div className="w-[97.4%] mx-[auto] mb-[14px] mt-[24px] flex gap-[14px]">
-        <CustomLink href="/peers/total-peers" label="Total Peers" />
-        <CustomLink href="/peers/top-peers" label="Top Peers" />
-        <CustomLink href="/peers/inactive-peers" label="Inactive Peers" />
-        <CustomLink href="/peers/active-peers" label="Active Peers" />
-        <CustomLink href="/peers/recommended-peers" label="Recommended Peers" />
-      </div>
-
-      <div className="w-[97.4%] mx-[auto] overflow-y-auto pt-[10px] pb-[69px]  no-scrollbar">
-        {children}
-      </div>
-    </div>
+    </>
   );
 };
 
